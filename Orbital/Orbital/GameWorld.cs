7196fa3cea87df2fd8585259613ede47d49fd8f8
Nichlas Hoberg <nichlashoberg@gmail.com>
2021-11-08 12:28:28 +0100

    Initial project commit
    Added vs project and all basic classes

---
+++ Orbital/Orbital/GameWorld.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Orbital
+{
+	public class GameWorld : Game
+	{
+		private GraphicsDeviceManager myGraphics;
+		private SpriteBatch mySpriteBatch;
+
+		public GameWorld()
+		{
+			myGraphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			mySpriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				Exit();
+			}
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
